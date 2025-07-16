eacd41557ca2cbe15cc23e2b65ab36f556c7b0fd
Mina Demian <mina@minademian.com>
2025-07-16 23:02:14 +0200

    feat(layout): cleanup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,12 @@ import { Image } from "expo-image";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { TextInput } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import styles from "../index-styles";
 
 export default function HomeScreen() {
-  const native = Gesture.Native();
+  const touch = Gesture.Tap();
 
   return (
     <ParallaxScrollView
@@ -21,17 +21,50 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">No Choice Alarm</ThemedText>
+        <ThemedText type="title">Get (dafak) Up :)</ThemedText>
       </ThemedView>
-      <ThemedText type="defaultSemiBold">set an alarm</ThemedText>
-      <GestureDetector gesture={native}>
-        <ThemedView style={styles.alarm}>
-          <ThemedText>Hour</ThemedText>
-          <TextInput placeholder="00"></TextInput>
-          <ThemedText>Minutes</ThemedText>
-          <TextInput placeholder="00"></TextInput>
-        </ThemedView>
-      </GestureDetector>
+      <ThemedView>
+        <GestureDetector gesture={touch}>
+          <ThemedView>
+            <View style={styles.alarm}>
+              <ThemedText type="subtitle">Hour</ThemedText>
+              <TextInput
+                style={styles.alarmInput}
+                placeholder="00"
+                keyboardType="number-pad"
+              ></TextInput>
+              <ThemedText type="subtitle">Minutes</ThemedText>
+              <TextInput
+                style={styles.alarmInput}
+                placeholder="00"
+                keyboardType="number-pad"
+              ></TextInput>
+              <Pressable
+                style={({ pressed }) => [
+                  {
+                    backgroundColor: pressed ? "rgb(169, 236, 169)" : "white",
+                  },
+                  styles.wrapperCustom,
+                  styles.addButton,
+                ]}
+              >
+                <ThemedText type="link" style={styles.addButtonLabel}>
+                  +
+                </ThemedText>
+              </Pressable>
+            </View>
+            <View style={styles.options}>
+              <ThemedText type="subheading">Select soundtrack</ThemedText>
+              <Pressable style={styles.optionsButton}>
+                <Text style={styles.label}>Music</Text>
+              </Pressable>
+              <Pressable style={styles.optionsButton}>
+                <Text style={styles.label}>Speech</Text>
+              </Pressable>
+            </View>
+          </ThemedView>
+        </GestureDetector>
+      </ThemedView>
     </ParallaxScrollView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import {
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
@@ -29,7 +28,6 @@ export default function RootLayout() {
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
-        <StatusBar style="auto" />
       </ThemeProvider>
     </GestureHandlerRootView>
   );
