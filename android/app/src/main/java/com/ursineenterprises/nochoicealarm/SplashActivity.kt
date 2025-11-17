0f7d25211b3b974a8e237b2af00ad9d0651db674
Mina Demian <mina@minademian.com>
2025-11-17 23:55:41 +0100

    feat: improvement for Android

---
+++ android/app/src/main/java/com/ursineenterprises/nochoicealarm/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.ursineenterprises.nochoicealarm
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launch_screen)
+        
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 3000)
+    }
+}
