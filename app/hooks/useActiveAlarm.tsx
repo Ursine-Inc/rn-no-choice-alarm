06a25a8403aab7c4c441c224b33286e704fae33e
Mina Demian <mina@minademian.com>
2025-11-19 09:58:24 +0100

    refactor/alarms persistence layer (#21)
    * ci: handle it locally
    * chore: update tracks
    * refactor: towards better track architecture and persistence layer
    * style: cleanup

--- app/hooks/useActiveAlarm.tsx
+++ app/hooks/useActiveAlarm.tsx
@@ -2,21 +2,9 @@ import {
   AudioFileName,
   MUSIC_AUDIO_SOURCES,
   SPEECH_AUDIO_SOURCES,
-} from "@/assets/data/tracks";
+} from "@/data/tracks";
+import { cleanFileName } from "@/utils/helpers";
 import { createContext, ReactNode, useContext, useMemo, useState } from "react";
-
-const cleanFileName = (fileName: string): string => {
-  const regex = new RegExp(" - Output - Stereo Out\\.(m4a|aac)$");
-  let cleaned = fileName.replace(regex, "");
-
-  cleaned = cleaned.replace(/^alarm\d+/i, "");
-
-  cleaned = cleaned.split("-")[0].trim();
-
-  cleaned = cleaned.charAt(0).toUpperCase() + cleaned.slice(1).toLowerCase();
-  return cleaned;
-};
-
 interface AudioMapping {
   fullFileName: AudioFileName;
   cleanName: string;
